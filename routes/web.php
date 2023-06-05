a4d7ed9ff1229fb9254f8ab87dc5150672927637
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-06-05 16:16:19 +0200

    UPDATE routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,9 +19,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(["auth", "verified"])->prefix("admin")->name("admin.")->group( function() {
+    Route::get('/', [DashboardController::class, "index"])->name('dashboard');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
