5f306067fb095d95ad45d53850ae7ca4b397c97e
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-06-06 11:42:04 +0200

    CHANGE modal style

--- resources/views/partials/deletionModal.blade.php
+++ resources/views/partials/deletionModal.blade.php
@@ -1,6 +1,6 @@
 <div class="deletionModal text-black p-5 rounded-4 shadow text-center off">
-  <h3>Deleting project "{{ $project->name }}"</h3>
-  <h6 class="mb-3">Are you sure you want to delete the project?</h6>
+  <h3 class="text-white">Deleting project "{{ $project->name }}"</h3>
+  <h6 class="mb-3 text-white">Are you sure you want to delete the project?</h6>
   <form action="{{ route('admin.projects.destroy', $project) }}" method="post">
     @csrf
     @method("delete")
