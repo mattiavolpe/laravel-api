c97465708847fb34d643b48d776c96d24983905d
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-06-07 15:49:37 +0200

    ADD type and projects methods

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Str;
 
 class Project extends Model
@@ -19,5 +20,9 @@ class Project extends Model
     public static function generateRepositoryUrl($slug) {
         $repositoryUrl = 'https://github.com/mattiavolpe/' . $slug;
         return $repositoryUrl;
+    }
+
+    public function type(): BelongsTo {
+        return $this->belongsTo(Type::class);
     }
 }
--- app/Models/Type.php
+++ app/Models/Type.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Type extends Model
 {
     use HasFactory;
+
+    public function projects(): HasMany {
+        return $this->hasMany(Projects::class);
+    }
 }
