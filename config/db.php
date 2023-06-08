448bc3310a290953804989859a7f7c7990584613
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-06-08 15:43:07 +0200

    ADD technologies array to seed the database

--- config/db.php
+++ config/db.php
@@ -3,5 +3,8 @@
   return [
     "types" => [
       "Front End", "Back End", "Full Stack"
+    ],
+    "technologies" => [
+      "HTML", "CSS", "JavaScript", "VueJS", "Vite", "PHP", "MySQL", "Laravel", "SCSS", "Bootstrap"
     ]
   ];
