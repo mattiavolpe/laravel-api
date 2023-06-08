21b105f7a5e26339300cb187145c40b18ef97de9
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-06-08 16:04:24 +0200

    ADD TechnologySeeder

--- database/migrations/2023_06_08_132843_create_project_technology_table.php
+++ database/migrations/2023_06_08_132843_create_project_technology_table.php
@@ -20,8 +20,6 @@ return new class extends Migration
             $table->unsignedBigInteger('technology_id');
             $table->foreign('technology_id')->references('id')->on('technologies')->cascadeOnDelete();
             $table->timestamps();
-
-            $table->primary(["project_id", "technology_id"]);
         });
     }
 
--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class TechnologySeeder extends Seeder
 {
@@ -14,6 +16,12 @@ class TechnologySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $technologies = config("db.technologies");
+        foreach ($technologies as $technology) {
+            $newTechnology = new Technology();
+            $newTechnology->name = $technology;
+            $newTechnology->slug = Str::slug($technology, "-");
+            $newTechnology->save();
+        }
     }
 }
