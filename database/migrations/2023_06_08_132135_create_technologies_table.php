669ca9c06e8567183b8d05379d877e8c1754935c
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-06-08 15:43:21 +0200

    ADD migration to create technologies table

--- database/migrations/2023_06_08_132135_create_technologies_table.php
+++ database/migrations/2023_06_08_132135_create_technologies_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('technologies', function (Blueprint $table) {
             $table->id();
+            $table->string("name", 100)->unique();
+            $table->string("slug", 100)->unique();
             $table->timestamps();
         });
     }
