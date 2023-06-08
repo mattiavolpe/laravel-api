bd0598ebfe9dad61cdb8c90d4de8edb851b57ced
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-06-08 15:42:18 +0200

    ADD technologies routes and controller

--- app/Http/Controllers/Admin/TechnologyController.php
+++ app/Http/Controllers/Admin/TechnologyController.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
 use App\Models\Technology;
 use App\Http\Requests\StoreTechnologyRequest;
 use App\Http\Requests\UpdateTechnologyRequest;
+use App\Http\Controllers\Controller;
 
 class TechnologyController extends Controller
 {
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Admin\ProjectController;
 use App\Http\Controllers\Admin\TypeController;
+use App\Http\Controllers\Admin\TechnologyController;
 use App\Models\Project;
 use Illuminate\Support\Facades\Route;
 
@@ -30,6 +31,9 @@ Route::middleware(["auth", "verified"])->prefix("admin")->name("admin.")->group(
     ]);
     Route::resource('/types', TypeController::class)->parameters([
         "types" => "type:slug"
+    ]);
+    Route::resource('/technologies', TechnologyController::class)->parameters([
+        "technologies" => "technology:slug"
     ]);
 });
 
