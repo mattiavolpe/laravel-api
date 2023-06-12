c51f9f4f18815ca69931e723754c36fd993ccc24
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-06-12 14:10:53 +0200

    ADD image preview on project deletion

--- resources/views/partials/projectDeletionModal.blade.php
+++ resources/views/partials/projectDeletionModal.blade.php
@@ -1,7 +1,12 @@
 <div class="deletionModal text-black p-5 rounded-4 shadow text-center off">
   <h3 class="text-white">Deleting project "{{ $project->name }}"</h3>
-  <h6 class="mb-3 text-white">Are you sure you want to delete the project?</h6>
-  <form action="{{ route('admin.projects.destroy', $project) }}" method="post">
+  <h6 class="mb-0 text-white">Are you sure you want to delete the project?</h6>
+  @if($project->image)
+  <div class="text-center">
+    <img width="100" src="{{ asset('storage/' . $project->image) }}" alt="{{ $project->name }} image">
+  </div>
+  @endif
+  <form class="mt-3" action="{{ route('admin.projects.destroy', $project) }}" method="post">
     @csrf
     @method("delete")
     <button type="submit" class="btn">Delete</button>
