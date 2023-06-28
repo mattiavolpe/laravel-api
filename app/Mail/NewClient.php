e22eb0f2d35e5a2fe22c323db142e716de353b67
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-06-28 17:06:52 +0200

    ADD mailable and basic mail view

---
+++ app/Mail/NewClient.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class NewClient extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $contact;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($contact)
+    {
+        $this->contact = $contact;
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            subject: 'New Client',
+            replyTo: $this->contact->emailAddress,
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return new Content(
+            view: 'view.name',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
+}
