3d7d5f048c7909d74c04679f90accfe069a741c3
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-11-22 22:51:16 +0100

    FIX seeder call() order

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,7 +21,7 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call([
-            ProjectSeeder::class, TypeSeeder::class
+            TypeSeeder::class, TechnologySeeder::class, ProjectSeeder::class
         ]);
     }
 }
