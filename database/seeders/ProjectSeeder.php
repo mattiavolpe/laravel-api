e1af3e7de3f65e1fcd89cabc6b7b2be1807f0584
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-11-23 01:24:05 +0100

    ADD other projects

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class ProjectSeeder extends Seeder
@@ -18,11 +17,12 @@ class ProjectSeeder extends Seeder
         $projects = config("db.projects");
         foreach ($projects as $project) {
             $newProject = new Project();
-            $newProject->id = $project["id"];
             $newProject->user_id = $project["user_id"];
             $newProject->type_id = $project["type_id"];
             $newProject->name = $project["name"];
-            $newProject->image = $project["image"];
+            if (array_key_exists("image", $project)) {
+                $newProject->image = $project["image"];
+            }
             $newProject->slug = Project::generateSlug($newProject->name);
             $newProject->repositoryUrl = $project["repositoryUrl"];
             $newProject->starting_date = $project["starting_date"];
